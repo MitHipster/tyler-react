d45909c605f99ea837fbb95c1ef799c9fa5da0f7
Tim Acker <tjacker31@gmail.com>
2018-04-03 22:38:40 -0400

    Results
    Starting building out the Results component of the project. Finished UI to follow.

---
+++ app/components/Results.js
@@ -0,0 +1,66 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+var queryString = require('query-string');
+var api = require('../utils/api');
+
+class Results extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true
+		};
+	}
+	componentDidMount() {
+		var players = queryString.parse(this.props.location.search);
+		api.battle([
+			players.playerOneName,
+			players.playerTwoName
+		]).then(function (results) {
+			if (!results) {
+				return this.setState(function () {
+					return {
+						error: 'Looks like there was an error. Check that both users exist on GitHub.',
+						loading: false
+					};
+				});
+			}
+			this.setState(function () {
+				return {
+					error: null,
+					winner: results[0],
+					loser: results[1],
+					loading: false
+				};
+			});
+		}.bind(this));
+	}
+	render() {
+		var error = this.state.error;
+		var winner = this.state.winner;
+		var loser = this.state.loser;
+		var loading = this.state.loading;
+		
+		if (loading) {
+			return <p>Loading</p>;
+		}
+
+		if (error) {
+			return (
+				<div>
+					<p>{error}</p>
+					<Link to='/battle'>Reset</Link>
+				</div>
+			);
+		}
+
+		return (
+			<div>{JSON.stringify(this.state, null, 2)}</div>
+		);
+	}
+}
+
+module.exports = Results;
